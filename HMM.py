7d91a1716a3ebec452f1873bd49512c65fde3400
ramtinms <ramtinms@yahoo.com>
2015-03-03 13:38:12 -0800

    cleaning

--- HMM.py
+++ HMM.py
@@ -1,70 +1,12 @@
 
 from sequence import Sequence
 from HMM_params import HMM_params
+import HMM_utils
+
 # seting up params 
 
 # Reading params from file
 # Reading sequence from file
-#Sequence()
-
-
-# Learning from data (supervised, learning)
-#train_set=Sequence("123456","ABCDEF")
-
-# always start from 1
-
-#for i in xrange(1,len(train_set)):
-#    print train_set[i],train_set.get_label(i)
-
-
-#def fw_bk_algorithm(self, sequence):
-
-
-#def train_supervised(self, sequence):
-
-def matrix_to_string(matrix, header=None):
-    """
-    Return a pretty, aligned string representation of a nxm matrix.
-
-    This representation can be used to print any tabular data, such as
-    database results. It works by scanning the lengths of each element
-    in each column, and determining the format string dynamically.
-
-    @param matrix: Matrix representation (list with n rows of m elements).
-    @param header: Optional tuple or list with header elements to be displayed.
-    """
-    if type(header) is list:
-        header = tuple(header)
-    lengths = []
-    if header:
-        for column in header:
-            lengths.append(len(column))
-    for row in matrix:
-        for column in row:
-            i = row.index(column)
-            column = str(column)
-            cl = len(column)
-            try:
-                ml = lengths[i]
-                if cl > ml:
-                    lengths[i] = cl
-            except IndexError:
-                lengths.append(cl)
-
-    lengths = tuple(lengths)
-    format_string = ""
-    for length in lengths:
-        format_string += "%-" + str(length) + "s "
-    format_string += "\n"
-
-    matrix_str = ""
-    if header:
-        matrix_str += format_string % header
-    for row in matrix:
-        matrix_str += format_string % tuple(row)
-
-    return matrix_str
-
 
 
 class HMM:
@@ -119,7 +61,7 @@ class HMM:
 
     def print_DP_table(self,sequence, table):
         header = [" "]+[sequence[i] for i in xrange(1,len(sequence)+1)]
-        print matrix_to_string(table,header)
+        print HMM_utils.matrix_to_string(table,header)
 
     def test(self):
         test_seq=Sequence(['killer','crazy','clown','problem'],["X"]*4)
